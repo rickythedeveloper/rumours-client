7dc1a2ea649ba6b4b2a6bea79aaa1ddf07a406cf
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-09-17 13:12:53 +0100

    Show dropdown when the text box is focused

--- src/components/generic/SearchDropdown.tsx/index.tsx
+++ src/components/generic/SearchDropdown.tsx/index.tsx
@@ -4,6 +4,7 @@ interface Props<T> {
 	getItemsData: (searchString: string) => T[];
 	getItemKey: (itemData: T, index: number) => string | number;
 	getElement: (itemData: T, index: number) => JSX.Element;
+	onHover?: (itemData: T, index: number) => void;
 	hoverHighlightColor?: string;
 	style?: CSSProperties;
 }
@@ -12,6 +13,7 @@ interface State<T> {
 	itemsData: T[];
 	items: JSX.Element[];
 	hoverIndex?: number;
+	isShown: boolean;
 }
 
 const styles: {[component: string]: React.CSSProperties} = {
@@ -32,8 +34,15 @@ export default class SearchDropdown<T> extends React.Component<Props<T>, State<T
 			searchString: '',
 			itemsData: [],
 			items: [],
+			isShown: false,
 		};
 		this.state = DEFAULT_STATE;
+	}
+
+	updateItems(searchString: string): void {
+		const itemsData = this.props.getItemsData(searchString);
+		const items = itemsData.map((itemData, index) => this.props.getElement(itemData, index));
+		this.setState({ searchString, items, itemsData });
 	}
 
 	render(): JSX.Element {
@@ -45,24 +54,40 @@ export default class SearchDropdown<T> extends React.Component<Props<T>, State<T
 					value={this.state.searchString}
 					onChange={(e) => {
 						const searchString = e.target.value;
-						const itemsData = this.props.getItemsData(searchString);
-						const items = itemsData.map((itemData, index) => this.props.getElement(itemData, index));
-						this.setState({ searchString, items });
+						this.updateItems(searchString);
 					}}
+					onFocus={(e) => {
+						const searchString = e.target.value;
+						this.updateItems(searchString);
+						this.setState({ isShown: true });
+					}}
+					onBlur={(e) => { this.setState({ isShown: false }); }}
 				/>
-				<div className="search-dropdown-options-container" style={styles.optionsContainer}>
-					{this.state.items.map((item, index) => (
-						<div
-							key={this.props.getItemKey(this.state.itemsData[index], index)}
-							style={{
-								backgroundColor: this.state.hoverIndex === index ? this.props.hoverHighlightColor || 'red' : undefined,
-							}}
-							onMouseEnter={() => { this.setState({ hoverIndex: index }); }}
-							onMouseLeave={() => { if (this.state.hoverIndex === index) this.setState({ hoverIndex: undefined }); }}
-						>
-							{item}
-						</div>
-					))}
+				<div
+					className="search-dropdown-options-container"
+					style={{
+						display: this.state.isShown ? undefined : 'none',
+						...styles.optionsContainer,
+					}}
+				>
+					{this.state.items.map((item, index) => {
+						const itemData = this.state.itemsData[index];
+						return (
+							<div
+								key={this.props.getItemKey(itemData, index)}
+								style={{
+									backgroundColor: this.state.hoverIndex === index ? this.props.hoverHighlightColor || 'red' : undefined,
+								}}
+								onMouseEnter={() => {
+									this.setState({ hoverIndex: index });
+									if (this.props.onHover) this.props.onHover(itemData, index);
+								}}
+								onMouseLeave={() => { if (this.state.hoverIndex === index) this.setState({ hoverIndex: undefined }); }}
+							>
+								{item}
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		);
